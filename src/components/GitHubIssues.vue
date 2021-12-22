6ded5f8254bbe7f503c4844424d3954259a10ba9
Yuri Oliveira <yurileader@gmail.com>
2021-12-22 19:35:18 -0300

    Testando a busca por issues,  e limpeza da tabela

--- src/components/GitHubIssues.vue
+++ src/components/GitHubIssues.vue
@@ -4,27 +4,35 @@
       <div class="row">
         <div class="col-5">
           <input
+            v-model="username"
             type="text"
             placeholder="GitHub usarname"
             class="form-control"
             id="gitUsername"
-            aria-describedby="github username"
           />
         </div>
         <div class="col-5">
           <input
+            v-model="repositorio"
             type="text"
             placeholder="GitHub repositório"
             class="form-control"
             id="gitRepositorio"
-            aria-describedby="git repositorio"
           />
         </div>
         <div class="col-2 d-flex">
-          <button type="button" class="btn btn-success btn-sm">
+          <button
+            @click.prevent.stop="buscarIssues()"
+            type="submit"
+            class="btn btn-success btn-sm"
+          >
             BUSCAR
           </button>
-          <button type="button" class="btn btn-danger btn-sm ms-1">
+          <button
+            @click.prevent.stop="limpar()"
+            type="button"
+            class="btn btn-danger btn-sm ms-1"
+          >
             LIMPAR
           </button>
         </div>
@@ -40,9 +48,13 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Mark</td>
-            <td>Otto</td>
+          <tr v-for="usr in userlist" :key="usr.id">
+            <td>{{ usr.id }}</td>
+            <td>{{ usr.body }}</td>
+          </tr>
+          <tr v-if="!userlist">
+            <td>#</td>
+            <td>Nenhuma issues encontrada!</td>
           </tr>
         </tbody>
       </table>
@@ -51,12 +63,42 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "GitHubIssues",
+
   data() {
-    return {};
+    return {
+      username: "",
+      repositorio: "",
+      user: !{
+        id: Number,
+        userId: Number,
+        title: String,
+        body: String
+      },
+      userlist: []
+    };
   },
-  methods: {}
+
+  methods: {
+    buscarIssues() {
+      console.log("BUSCOU");
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+          this.userlist = [...response.data];
+          console.log(this.userlist);
+        })
+        .catch(erro => console.log(erro));
+    },
+
+    limpar() {
+      console.log("Limpou");
+      (this.username = ""), (this.repositorio = ""), (this.userlist = []);
+    }
+  }
 };
 </script>
 
