1918e282d349217cf42b8d379e3c597de80f9be8
Yuri Oliveira <yurileader@gmail.com>
2021-12-24 12:47:48 -0300

    Adicionando rota  para carregar issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GitHubIssues from '../components/GitHubIssues.vue'
+import GitHubIssue from '../components/GitHubIssue.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'GitHubIssues',
       component: GitHubIssues
+    },
+    {
+      path: '/:name/:repo/:issue',
+      name: 'GitHubIssue',
+      component: GitHubIssue
     }
   ]
 })
